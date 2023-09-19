43522b863f1c89a75f4b9a85d33b63f1b5004613
Nedya Ibrahim <Nedyaqr@gmail.com>
2023-09-19 12:57:36 +0200

    update

--- Exercies/övning.py
+++ Exercies/övning.py
@@ -1,24 +1,7 @@
-EXPECTED_BAKE_TIME = 40 
-def bake_time_remaining(elapsed_bake_time:int)->int:
-    time_remaining = EXPECTED_BAKE_TIME - elapsed_bake_time
-    return time_remaining
 
-PREPARATION_TIME = 2
-
-def preparation_time_in_minutes(number_of_layers):
-    time_to_prepare = number_of_layers * PREPARATION_TIME
-    return time_to_prepare
-
-    time_to_prepair = EXPECTED_BAKE_TIME = 40 
-
-def bake_time_remaining(elapsed_bake_time:int)->int:
-    time_remaining = EXPECTED_BAKE_TIME - elapsed_bake_time
-
-    return time_remaining
-
-
-def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
-    total_elapsed_time = preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
-    return total_elapsed_time
-
-
+def square(integer_number):
+    if not 1 <= integer_number <= 64:
+        raise ValueError('square must be between 1 and 64')
+    return 1 << (integer_number - 1)
+def total():
+    return (1 << 64) - 1
